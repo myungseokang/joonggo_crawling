aad864296e46956b9a30cfedfd0a93f89dd299ee
Leop0ld <l3opold7@gmail.com>
2017-02-25 15:50:28 +0900

    Modify CSS selector

--- joonggo_crawling.py
+++ joonggo_crawling.py
@@ -12,11 +12,13 @@ def get_response_text(url, **kwargs):
 
 
 def crawl_joonggo():
-    url = 'http://cafe.naver.com/ArticleList.nhn?search.clubid=10050146&search.menuid=387&search.page=1'
+    url = 'http://cafe.naver.com/ArticleList.nhn?search.clubid=10050146&search.menuid=387&search.page=2'
     resp = get_response_text(url)
 
     html = BeautifulSoup(resp, 'lxml')
-    tr_list = html.select('form[name="ArticleList"] tr[align="center"] td[class="board-list"] span[class="aaa"] a')
+    tr_list = html.select(
+        'form[name="ArticleList"] tr[align="center"] td[class="board-list"] span[class="aaa"] a[onmouseover]'
+    )
 
     for tr in tr_list:
         print(tr.text)
